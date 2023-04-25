e9be07e06d64694531b6c5a787c23b356a934f25
CantinDeBrunoy <cantin.roquier@gmail.com>
2023-04-25 15:01:52 +0200

    feat(backEnd) new services : getMateriels,addUsers,addMateriels

--- backend/database.js
+++ backend/database.js
@@ -15,3 +15,19 @@ export async function getUsers(){
     const request = await pool.query("SELECT * from utilisateur");
     return request[0];
 }
+
+
+export async function getMateriels(){
+    const request = await pool.query("SELECT * from materiel");
+    return request[0];
+}
+
+export async function addUtilisateur(utilisateur){ 
+    const result = await pool.query(`INSERT INTO utilisateur (nom, prenom, adresse_mail, mot_de_passe, role) VALUES ('${utilisateur.nom}', '${utilisateur.prenom}', '${utilisateur.adresse_mail}', '${utilisateur.mot_de_passe}', 'utilisateur')`);
+    return result[0];
+}
+
+export async function addMateriel(materiel){ 
+    const result = await pool.query(`INSERT INTO materiel (nom, categorie, prix, marque, quantite) VALUES ('${materiel.nom}', '${materiel.categorie}', ${materiel.prix}, '${materiel.marque}', ${materiel.quantite})`);
+    return result[0];
+}
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import { getUsers } from "./database.js";
+import { getUsers ,getMateriels,addUtilisateur,addMateriel} from "./database.js";
 
 const app = express();
 const port = process.env.PORT;
@@ -10,11 +10,45 @@ app.use(cors());
 app.use(express.json());
 dotenv.config();
 
-//requetes kekettes
-
+//RecupUser
 app.get("/getUtilisateurs",async(req,res)=> {
     const personnes = await getUsers();
     res.send(personnes);
 })
+
+//getMateriel
+app.get("/getMateriels",async(req,res)=> {
+    const materiels = await getMateriels();
+    res.send(materiels);
+})
+
+//AjoutPersonne
+app.post('/AjoutUtilisateur',async(req,res)=> {
+    const utilisateur = {
+        nom : 'Didi',
+        prenom : 'Didier',
+        adresse_mail : 'Didier.Didi@gmail.com',
+        mot_de_passe : 'mdp',
+        role: 'utilisateur'
+    };
+    const user = await addUtilisateur(utilisateur);
+    res.send(user);
+});
+
+//AjoutMateriel
+app.post('/AjoutMateriel',async(req,res)=> {
+    const materielTmp = {
+        nom : 'truelle',
+        categorie : 'bricolage',
+        prix : 15,
+        marque : 'Leroy Merlin',
+        quantite: 5
+    };
+
+    const materiel = await addMateriel(materielTmp);
+    res.send(materiel);
+});
+
+
 
 app.listen(port,()=> console.log(`app is running on port ${port} :D`));
