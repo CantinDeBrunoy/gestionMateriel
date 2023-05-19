a4c34cf676345a8bf620a69a99ce5c4384a110ad
vharaphis <muttifrancois@gmail.com>
2023-05-19 13:06:18 +0200

    style(responsive) responsive design/ticket

--- frontend/src/components/Ticket.js
+++ frontend/src/components/Ticket.js
@@ -1,7 +1,20 @@
+import { useState } from "react";
 import NavBar from "./navBar/NavBar";
+import RechercheMateriel from "./rechercheMateriel/RechercheMateriel";
 import "./Ticket.css";
+import * as React from "react";
 
 function Ticket() {
+    const [displayListMateriel, setDisplayListMateriel] = React.useState(false);
+    const showListMateriel = () => setDisplayListMateriel(!displayListMateriel);
+
+    const current = new Date();
+    const actualDate = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
+
+    const [listItems,setListItems] = useState([
+        'aaaa','aaa','ab','abc','aabc','dsqdsq','dsqd','dsqdsq','trqsdqs','tfdqs'
+    ])
+
     return (
         <div>
             <NavBar/>
@@ -14,13 +27,21 @@ function Ticket() {
                         placeholder="Description du ticket (ce que vous souhaitez faire avec le matériel)"/>
                     <div className="Ticket-date">
                         <span>Début de l'emprunt : </span>
-                        <input type="date"/>
+                        <input type="date" min={actualDate}/>
                         <br/>
                         <span>Fin de l'emprunt : </span>
-                        <input type="date"/>
+                        <input type="date" min={actualDate}/>
                     </div>
+                    <button onClick={showListMateriel} className="Ticket-show-materiel orange">Choisir ce que j'emprunte</button>
+                    <span>/*Afficher nom des items slectionné ici*/</span>
+                    <button className="ButtonForm green Ticket-submit">Envoyer</button>
                 </div>
-                <button>Envoyer</button>
+                {displayListMateriel ? 
+                    <div className="Ticket-materiel">
+                        <h3>Le matériel que j'emprunte : </h3>
+                        <RechercheMateriel listItems={listItems}/>
+                    </div> : null
+                }
             </div>
         </div>
     )
